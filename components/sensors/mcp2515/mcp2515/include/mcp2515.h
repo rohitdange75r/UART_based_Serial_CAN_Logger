e91abbf59ab5dcc65ada462e7db684771a479d4d
Rohit Dange <224847085+rohitdange75r@users.noreply.github.com>
2026-06-21 01:12:01 +0530

    added MCP2515 header

---
+++ components/sensors/mcp2515/mcp2515/include/mcp2515.h
@@ -0,0 +1,13 @@
+#ifndef MCP2515_H
+#define MCP2515_H
+
+#include "spi_driver.h"
+#include "esp_err.h"
+
+esp_err_t mcp2515_init(spi_bus_device_handle_t spi_link, int interrupt_pin);
+void mcp2515_reset(void);
+uint8_t mcp25115_read_register(uint8_t reg_addr);
+void mcp2515_write_register(uint8_t reg_addr, uint8_t value);
+void mcp2515_bit_modify(uint8_t reg_addr, uint8_t mask, uint8_t value);
+
+#endif
