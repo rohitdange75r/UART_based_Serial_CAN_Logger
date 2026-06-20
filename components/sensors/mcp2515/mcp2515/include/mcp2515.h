03211abcc0de4ff56bce68500abfa9049e12eb35
Rohit Dange <224847085+rohitdange75r@users.noreply.github.com>
2026-06-21 01:12:12 +0530

    Fixed the MCP2515 read function naming

--- components/sensors/mcp2515/mcp2515/include/mcp2515.h
+++ components/sensors/mcp2515/mcp2515/include/mcp2515.h
@@ -6,7 +6,7 @@
 
 esp_err_t mcp2515_init(spi_bus_device_handle_t spi_link, int interrupt_pin);
 void mcp2515_reset(void);
-uint8_t mcp25115_read_register(uint8_t reg_addr);
+uint8_t mcp2515_read_register(uint8_t reg_addr);
 void mcp2515_write_register(uint8_t reg_addr, uint8_t value);
 void mcp2515_bit_modify(uint8_t reg_addr, uint8_t mask, uint8_t value);
 
